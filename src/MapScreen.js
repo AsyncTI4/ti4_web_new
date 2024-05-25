eef922e7889192005006b74ce20c93a41e9cb979
Brendan Speer <brendan.speer@gmail.com>
2024-05-25 13:38:21 -0500

    * initial commit

---
+++ src/MapScreen.js
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from "react";
+import { useParams } from "react-router-dom";
+
+function MapScreen() {
+   const params = useParams()
+   const imgref = useRef();
+   useEffect(() => {
+    fetch("https://ti4.westaddisonheavyindustries.com/maps.json")
+      .then((res) => {
+        return res.json()
+      })
+      .then((pl) => {
+        console.log(pl)
+        const u = pl.find(x => x.MapName === params.mapid).MapURL
+        imgref.current.src=u
+      })
+   })
+   
+   return <div><img ref={imgref}></img></div>
+}
+
+export default MapScreen;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import MapScreen from './MapScreen';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>TI4 Web mk. 2</div>,
+  },
+  {
+    path: "/map/:mapid",
+    element: <MapScreen />
+  },
+]);
+
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>
 );
 
