48fe7f39126d8f6563b8b80e12f95221381eb7a9
James Strong <54325531+heisenbugged@users.noreply.github.com>
2024-09-25 12:54:53 -0500

    Better pin/zoom/pinch

--- src/MapScreen.jsx
+++ src/MapScreen.jsx
@@ -7,21 +7,19 @@ import {
   useMantineTheme,
 } from "@mantine/core";
 import "./MapScreen.css";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { Atom } from "react-loading-indicators";
 import logo from "./assets/banner.png";
 import { useMaps } from "./useMaps";
 import { useMapSocket } from "./useMapSocket";
-import { ZoomControls } from "./ZoomControls";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
-function MapScreen() {
+function MapScreen(props) {
   const navigate = useNavigate();
   const params = useParams();
   const theme = useMantineTheme();
 
-  const [zoom, setZoom] = useState(1);
-  const imageRef = useRef(null);
   const [imageUrl, setImageUrl] = useState(null);
   const [activeTabs, setActiveTabs] = useState([]);
 
@@ -60,33 +58,74 @@ function MapScreen() {
     }
   };
 
-  const handleZoomIn = () => {
-    setZoom((prevZoom) => {
-      if (prevZoom === undefined) return 1;
-      return Math.min(prevZoom + 0.25, 2);
-    });
-  };
-
-  const handleZoomOut = () => {
-    setZoom((prevZoom) => {
-      if (prevZoom === undefined) return 1;
-      return Math.max(prevZoom - 0.25, 0.5);
-    });
-  };
-
-  const handleZoomReset = () => {
-    setZoom(undefined);
-  };
-
-  const zoomPercent = zoom
-    ? (zoom * 100).toFixed(0).toString() + "%"
-    : undefined;
-
   const defaultImageUrl = mapsQuery.data?.find(
     (v) => v.MapName === params.mapid
   )?.MapURL;
 
   const derivedImageUrl = imageUrl ?? defaultImageUrl;
+
+  const [container, setContainer] = useState(null);
+
+  const [containerWidth, setContainerWidth] = useState(0);
+  const [containerHeight, setContainerHeight] = useState(0);
+
+  const [imageNaturalWidth, setImageNaturalWidth] = useState(0);
+  const [imageNaturalHeight, setImageNaturalHeight] = useState(0);
+
+  const scaleUp = true;
+  const zoomFactor = 8;
+
+  const imageScale = useMemo(() => {
+    if (
+      containerWidth === 0 ||
+      containerHeight === 0 ||
+      imageNaturalWidth === 0 ||
+      imageNaturalHeight === 0
+    )
+      return 0;
+
+    const scale = containerWidth / imageNaturalWidth;
+    return scaleUp ? scale : Math.max(scale, 1);
+  }, [
+    scaleUp,
+    containerWidth,
+    containerHeight,
+    imageNaturalWidth,
+    imageNaturalHeight,
+  ]);
+
+  const handleResize = useCallback(() => {
+    if (container !== null) {
+      const rect = container.getBoundingClientRect();
+      setContainerWidth(rect.width);
+      setContainerHeight(rect.height);
+    } else {
+      setContainerWidth(0);
+      setContainerHeight(0);
+    }
+  }, [container]);
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [handleResize]);
+
+  const handleImageOnLoad = (image) => {
+    setImageNaturalWidth(image.naturalWidth);
+    setImageNaturalHeight(image.naturalHeight);
+  };
+
+  useEffect(() => {
+    if (!derivedImageUrl) return;
+    const image = new Image();
+    image.onload = () => handleImageOnLoad(image);
+    image.src = derivedImageUrl;
+  }, [derivedImageUrl]);
+
+  console.log('derivedImageUrl', derivedImageUrl)
 
   return (
     <AppShell header={{ height: 60 }}>
@@ -147,26 +186,16 @@ function MapScreen() {
           style={{ color: "white", backgroundColor: "#222" }}
         >
           <div className="imageContainer">
-            <ZoomControls
-              zoom={zoom}
-              onZoomIn={handleZoomIn}
-              onZoomOut={handleZoomOut}
-              onZoomReset={handleZoomReset}
-            />
-
-            {derivedImageUrl ? (
-              <img
-                ref={imageRef}
-                alt="map"
-                src={derivedImageUrl}
-                style={{
-                  width: zoomPercent,
-                  imageRendering: "crisp-edges",
-                }}
-              />
-            ) : (
-              <div
-                style={{
+            <div
+              style={{
+                width: "100%",
+                height: "100%",
+              }}
+              ref={(el) => setContainer(el)}
+            >
+              {!derivedImageUrl ? (
+                <div
+                  style={{
                   display: "flex",
                   justifyContent: "center",
                   alignItems: "center",
@@ -178,9 +207,32 @@ function MapScreen() {
                   color={theme.colors.blue[5]}
                   size="large"
                   text="Loading"
-                />
-              </div>
-            )}
+                  />
+                </div>
+              ) : undefined}
+
+              {imageScale > 0 ? (
+                <TransformWrapper
+                  key={`${containerWidth}x${containerHeight}`}
+                  initialScale={imageScale}
+                  minScale={imageScale * 0.5}
+                  maxScale={imageScale * zoomFactor}
+                >
+                  <TransformComponent
+                    wrapperStyle={{
+                      width: "100%",
+                      height: "100%",
+                    }}
+                  >
+                    {derivedImageUrl ? (
+                      <img alt="map" src={derivedImageUrl} />
+                    ) : undefined}
+                  </TransformComponent>
+                </TransformWrapper>
+              ) : undefined}
+
+
+            </div>
           </div>
         </div>
       </AppShell.Main>
