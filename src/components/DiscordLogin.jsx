8bf66fe2a5551db81d6b689a85b71b88ba3de263
James Strong <54325531+heisenbugged@users.noreply.github.com>
2024-10-11 23:39:59 -0500

    Update DiscordLogin.jsx

--- src/components/DiscordLogin.jsx
+++ src/components/DiscordLogin.jsx
@@ -4,8 +4,8 @@ import { IconBrandDiscordFilled } from "@tabler/icons-react";
 
 export const getDiscordOauthUrl = () => {
   return import.meta.env.DEV
-    ? "https://discord.com/oauth2/authorize?client_id=1084164538053689464&response_type=code&redirect_uri=https://ti4.westaddisonheavyindustries.com/login&scope=identify%20guilds%20guilds.members.read"
-    : "https://ti4.westaddisonheavyindustries.com/maps.json";
+    ? "https://discord.com/oauth2/authorize?client_id=1286176779643654175&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Flogin&scope=identify%20guilds%20guilds.members.read"
+    : "https://discord.com/oauth2/authorize?client_id=1084164538053689464&response_type=code&redirect_uri=https://ti4.westaddisonheavyindustries.com/login&scope=identify%20guilds%20guilds.members.read";
 };
 
 export function DiscordLogin() {
