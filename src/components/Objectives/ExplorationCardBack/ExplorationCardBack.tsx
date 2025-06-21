24d951f3e84136e1f40ba00a2b7a98fe32769d37
Wezabi <Westonhuhman@gmail.com>
2025-06-20 19:13:08 -0500

    Initial commit of exploration deck details card

---
+++ src/components/Objectives/ExplorationCardBack/ExplorationCardBack.tsx
@@ -0,0 +1,33 @@
+import { Stack } from "@mantine/core";
+import { useState } from "react";
+import { Cardback } from "../../PlayerArea/Cardback";
+import { cdnImage } from "@/data/cdnImage";
+import { SmoothPopover } from "../../shared/SmoothPopover";
+import { ExplorationDetailsCard } from "../ExplorationDetailsCard";
+import styles from "./ExplorationCardBack.module.css";
+
+type Props = {
+  type: string;
+  deck: string[];
+  discard: string[];
+};
+
+export function ExplorationCardBack({ type, deck, discard }: Props) {
+  const [opened, setOpened] = useState(false);
+
+  return (
+    <SmoothPopover opened={opened} onChange={setOpened}>
+      <SmoothPopover.Target>
+        <Stack
+          className={`${styles.card}`}
+          onClick={() => setOpened((o) => !o)}
+        >
+          <Cardback key={type} src={cdnImage(`/player_area/cardback_${type.toLowerCase()}.jpg`)} alt={`${type} explore`} count={deck.length} size="lg"/>
+        </Stack>
+      </SmoothPopover.Target>
+      <SmoothPopover.Dropdown className={styles.popoverDropdown}>
+        <ExplorationDetailsCard type={type} deck={deck} discard={discard} />
+      </SmoothPopover.Dropdown>
+    </SmoothPopover>
+  );
+}
---
+++ src/components/Objectives/ExplorationDetailsCard/index.ts
@@ -0,0 +1,1 @@
+export { ExplorationDetailsCard } from "./ExplorationDetailsCard";
