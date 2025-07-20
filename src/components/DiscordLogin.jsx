6cb629054dfeb155d48fc08e42a93370638bdf42
Kredden <brendan.speer@gmail.com>
2025-07-20 14:38:26 -0500

    Changes to discord login to follow website config. (#59)

--- src/components/DiscordLogin.jsx
+++ src/components/DiscordLogin.jsx
@@ -1,11 +1,12 @@
 import { Button, Group, Text } from "@mantine/core";
 import { useUser } from "../hooks/useUser";
 import { IconBrandDiscordFilled } from "@tabler/icons-react";
+import { config } from "../config";
 
 export const getDiscordOauthUrl = () => {
   return import.meta.env.DEV
     ? "https://discord.com/oauth2/authorize?client_id=1286176779643654175&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Flogin&scope=identify%20guilds%20guilds.members.read"
-    : "https://discord.com/oauth2/authorize?client_id=1084164538053689464&response_type=code&redirect_uri=https://ti4.westaddisonheavyindustries.com/login&scope=identify%20guilds%20guilds.members.read";
+    : `https://discord.com/oauth2/authorize?client_id=1084164538053689464&response_type=code&redirect_uri=${config.api.websiteBase}login&scope=identify%20guilds%20guilds.members.read`;
 };
 
 export function DiscordLogin() {
