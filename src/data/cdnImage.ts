3b024f4c3a042968be8125401650e62991de4687
James Strong <54325531+heisenbugged@users.noreply.github.com>
2025-10-27 06:54:01 -0500

    player pannable fix + cache bust

--- src/data/cdnImage.ts
+++ src/data/cdnImage.ts
@@ -1,4 +1,4 @@
 export function cdnImage(image: string) {
   const webpImage = image.replace(/\.(png|PNG|jpg|JPG)$/, ".webp");
-  return `https://images.asyncti4.com${webpImage}?v=1`;
+  return `https://images.asyncti4.com${webpImage}?v=2`;
 }
