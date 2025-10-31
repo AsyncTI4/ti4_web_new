4c40ea851c6f41192a116f34bc2390bc0ef08465
James Strong <54325531+heisenbugged@users.noreply.github.com>
2025-10-31 15:13:04 -0500

    Update cdnImage.ts

--- src/data/cdnImage.ts
+++ src/data/cdnImage.ts
@@ -1,4 +1,4 @@
 export function cdnImage(image: string) {
   const webpImage = image.replace(/\.(png|PNG|jpg|JPG)$/, ".webp");
-  return `https://images.asyncti4.com${webpImage}?v=2`;
+  return `https://images.asyncti4.com${webpImage}?v=3`;
 }
