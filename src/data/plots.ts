1cd81ea58d1a9cb9a420360b705e1e33ff6a5ca1
RobertGuglielmino <robert.gugliel@gmail.com>
2025-11-04 15:21:01 -0500

    plot data

---
+++ src/data/plots.ts
@@ -0,0 +1,34 @@
+import { PlotCardInfo } from "./types"
+
+export const plots: PlotCardInfo[] = [
+  {
+    identifier: 0,
+    name: "Enervate",
+    text: "You can perform the secondary abilities of the puppeted players strategy cards without spending command tokens. For Leadership, you can perform the primary ability instead.",
+    source: "te",
+  },
+  {
+    identifier: 1,
+    name: "Syphon",
+    text: "When the puppeted player gains commodities, you gain an equal number of trade goods.",
+    source: "te",
+  },
+  {
+    identifier: 2,
+    name: "Seethe",
+    text: "When this card is revealed, destroy all units on a non-home planet controlled by the puppeted player. At the start of the status phase, destroy one of the puppeted players infantry in any system.",
+    source: "te",
+  },
+  {
+    identifier: 3,
+    name: "Asail",
+    text: "Apply +1 to the results of each of your combat and unit ability rolls against the puppeted player.",
+    source: "te",
+  },
+  {
+    identifier: 4,
+    name: "Extract",
+    text: "When this card is revealed, gain one non-faction technology owned by the puppeted player. When the puppeted player gains a non-faction technology, you may spend 4 resources to gain that technology.",
+    source: "te",
+  },
+]
